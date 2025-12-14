3e336bef1eddb6f642e28a01484ef086e2dfd8f7
Mario Cagalj <mcagalj@fesb.hr>
2025-12-14 10:40:10 +0100

    feat: Install and integrate Better Auth, run and fix db migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'drizzle-kit';
 config({ path: '.env' });
 
 export default defineConfig({
-  schema: './db/schema.ts',
+  schema: ['./db/schema.ts', './db/auth-schema.ts'],
   out: './db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
